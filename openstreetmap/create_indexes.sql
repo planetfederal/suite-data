8bade610f0c3abe009cf974903ef06ce607b8aa7
jdeolive <jdeolive@opengeo.org>
2013-05-15 10:59:34 -0600

    added script for db index creation

---
+++ openstreetmap/create_indexes.sql
@@ -0,0 +1,20 @@
+CREATE INDEX ON planet_osm_point ( place );
+CREATE INDEX ON planet_osm_point ( capital );
+
+CREATE INDEX ON planet_osm_polygon ( waterway );
+CREATE INDEX ON planet_osm_polygon ( tourism );
+CREATE INDEX ON planet_osm_polygon ( natural );
+CREATE INDEX ON planet_osm_polygon ( leisure );
+CREATE INDEX ON planet_osm_polygon ( landuse );
+CREATE INDEX ON planet_osm_polygon ( highway );
+CREATE INDEX ON planet_osm_polygon ( building );
+CREATE INDEX ON planet_osm_polygon ( barrier );
+CREATE INDEX ON planet_osm_polygon ( amenity );
+
+CREATE INDEX ON planet_osm_line ( aeroway );
+CREATE INDEX ON planet_osm_line ( bridge );
+CREATE INDEX ON planet_osm_line ( highway );
+CREATE INDEX ON planet_osm_line ( man_made );
+CREATE INDEX ON planet_osm_line ( railway );
+CREATE INDEX ON planet_osm_line ( waterway );
+CREATE INDEX ON planet_osm_line ( z_order );
